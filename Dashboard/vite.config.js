42a0956b91b78f4d6ab94f39763109dc47c2dcd1
DemianVGLl2 <0253139@up.edu.mx>
2026-04-16 19:18:34 -0600

    feat: Attempt for live web host

--- Dashboard/vite.config.js
+++ Dashboard/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SG1_Team1/',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+  },
 })
